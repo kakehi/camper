b8d4a088057d4d08624c2b0d5d0cbec6734d163a
Takuma Kakehi <takuma.kakehi@gmail.com>
2023-09-09 07:25:46 -0400

    helper to access age group with camp, considering all the activities ages (in progress)

--- app/helpers/camps_helper.rb
+++ app/helpers/camps_helper.rb
@@ -5,4 +5,25 @@ module CampsHelper
             "Single theme camp"
         ]        
     end
+
+    def camp_get_age_group_from_activities(camp)
+        activities = Activity.where(camp_id: camp.id)
+        age_group_min = nil
+        age_group_max = nil
+        activities.each do |activity|
+            if activity.age_group_min.present?
+                age_group_min = activity.age_group_min
+            end
+            if activity.age_group_max.present?
+                age_group_max = activity.age_group_max
+            end
+        end
+
+        age_group = age_group_min.nil? ? 
+            "" : "#{age_group_options[age_group_min]}"
+        age_group = age_group_max.nil? ? 
+            age_group : "#{age_group} to #{age_group_options[age_group_max]}"
+
+        return age_group
+    end
 end
