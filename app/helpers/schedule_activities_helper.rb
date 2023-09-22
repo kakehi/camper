1d23005ca17392853da38dce19616484a66c84e3
Takuma Kakehi <takuma.kakehi@gmail.com>
2023-09-22 16:05:51 -0400

    profile groups activties in the schedule

--- app/helpers/schedule_activities_helper.rb
+++ app/helpers/schedule_activities_helper.rb
@@ -1,2 +1,68 @@
 module ScheduleActivitiesHelper
+
+    def schedule_summer_templates
+        [
+            {
+                year: 2023,
+                weeks: [
+                    {
+                        start_at: Date.new(2023, 6, 12),
+                        end_at: Date.new(2023, 6, 16)
+                    },
+                    {
+                        start_at: Date.new(2023, 6, 19),
+                        end_at: Date.new(2023, 6, 23)
+                    },
+                    {
+                        start_at: Date.new(2023, 6, 26),
+                        end_at: Date.new(2023, 6, 30)
+                    },
+                    {
+                        start_at: Date.new(2023, 7, 3),
+                        end_at: Date.new(2023, 7, 7)
+                    },
+                    {
+                        start_at: Date.new(2023, 7, 10),
+                        end_at: Date.new(2023, 7, 14)
+                    },
+                    {
+                        start_at: Date.new(2023, 7, 17),
+                        end_at: Date.new(2023, 7, 21)
+                    },
+                    {
+                        start_at: Date.new(2023, 7, 24),
+                        end_at: Date.new(2023, 7, 28)
+                    },
+                    {
+                        start_at: Date.new(2023, 7, 31),
+                        end_at: Date.new(2023, 8, 4)
+                    },
+                    {
+                        start_at: Date.new(2023, 8, 7),
+                        end_at: Date.new(2023, 8, 11)
+                    },
+                    {
+                        start_at: Date.new(2023, 8, 14),
+                        end_at: Date.new(2023, 8, 18)
+                    },
+                    {
+                        start_at: Date.new(2023, 8, 21),
+                        end_at: Date.new(2023, 8, 25)
+                    },
+                    {
+                        start_at: Date.new(2023, 8, 28),
+                        end_at: Date.new(2023, 9, 1)
+                    }
+                ]
+            }
+        ]        
+    end
+
+    def get_summer_templates(years)
+        schedule_summer_templates.select{|e| years.select{|y| y == e[:year]}.count > 0} 
+    end
+
+    def get_current_summer
+        get_summer_templates([2023]).first()
+    end
 end
