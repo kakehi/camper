7a3e7535f0f074cfb95348b9bb2ef612e9e21e89
Takuma Kakehi <takuma.kakehi@gmail.com>
2023-09-22 06:10:27 -0400

    in profile scheduled thread is organized properly

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -15,4 +15,20 @@ module OrganizationsHelper
             },
         ]        
     end
+
+
+    def get_organization_href(organization)
+        camps = Camp.where(organization_id: organization.id)
+        camp = camps.first
+    
+    
+        if camps.count == 1 && camp.present?
+            href = index_camp_activities_path(organization, camp)
+        else
+            href = index_organization_camp_path(organization)
+        end
+
+        href
+    end
+
 end
